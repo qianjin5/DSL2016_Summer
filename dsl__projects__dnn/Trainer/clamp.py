1a422289510f4c5dde970139116580742cab04ea
qianjin5 <joeqian96@hotmail.com>
2016-08-31 11:36:58 -0400

    8/31/2016

--- dsl__projects__dnn/Trainer/clamp.py
+++
@@ -1,7 +0,0 @@
-def clamp(value, lb, ub):
-    # Takes a value, a lower bound (lb) and an upper bound (ub) and returns the
-    # value if it is between the lb and ub, the lb if the value is below the
-    # lb, and the ub if the value is above the upper bound.
-    result = max(min(value, ub), lb)
-    return result
-
